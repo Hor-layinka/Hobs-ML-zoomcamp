545c75015d593a2b22f9f1508a08e0535c73d854
Abioye Hamid <76201781+Hor-layinka@users.noreply.github.com>
2024-06-22 16:18:20 +0000

    changes with db name

---
+++ 05_monitoring/dummy_metrics_calculation.py
@@ -0,0 +1,69 @@
+import datetime
+import random
+import time
+import logging
+import uuid
+import pytz
+import pandas as pd
+import io
+import psycopg
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
+
+SENd_tiMEOUT = 10
+rand = random.Random()
+
+create_table_statement = """ 
+drop table if exists dummy_metrics;
+create table dummy_metrics(
+	timestamp timestamp,
+	value1 integer,
+	value2 varchar,
+	value3 float
+)
+"""
+
+# create database and table for dummy variabbles
+
+def prep_db():
+	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
+		res = conn.execute("SELECT 1 FROM pg_database WHERE datname='test'")
+		if len(res.fetchall()) == 0:
+			conn.execute("create database test;")
+		with psycopg.connect("host=localhost port=5432 user=postgres password=example") as conn:
+			conn.execute(create_table_statement)
+
+# calcilate dummy metrics values
+def calculate_dummy_metrics_postgres(curr):
+	value1 = rand.randint(0, 1000)
+	value2 = str(uuid.uuid4())
+	value3 = rand.random()
+
+	# insert the  calculated values
+	curr.execute(
+		"insert into dummy_metrics(timestamp,value1, value2, value3) values (%s,%s,%s,%s)",
+		(datetime.datetime.now(pytz.timezone('Europe/London')), value1, value2, value3)
+	)
+
+# iterate to insert values
+
+def main():
+	prep_db()
+	last_send = datetime.datetime.now()-datetime.timedelta(seconds=10)
+	with psycopg.connect("host=localhost port=5432 user=postgres password=example", autocommit=True) as conn:
+		for i in range(0,10):
+			with conn.cursor() as curr:
+				calculate_dummy_metrics_postgres(curr)
+
+			new_send = datetime.datetime.now()
+			seconds_elapsed = (new_send - last_send).total_seconds()
+			if seconds_elapsed < SENd_tiMEOUT:
+				time.sleep(SENd_tiMEOUT - seconds_elapsed)
+			while last_send < new_send:
+				last_send = last_send + datetime.timedelta(seconds=10)
+			logging.info('data sent')
+
+
+if __name__ == '__main__':
+	main()
+
